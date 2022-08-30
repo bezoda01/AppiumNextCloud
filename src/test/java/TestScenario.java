2a1fbc1af04a9caf92139315c9332bc417d55b41
bezoda01 <86252841+bezoda01@users.noreply.github.com>
2022-08-30 10:26:46 +0400

    base

---
+++ src/test/java/TestScenario.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+import utils.StringUtils;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public class TestScenario extends BaseClass {
+
+    @Test
+    public void test() {
+
+    }
+}
